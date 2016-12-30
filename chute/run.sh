2d2c784636e07ac0bd2f87b2bf71a8911060e1ae
Lance Hartung <lance@paradrop.io>
2016-12-30 14:34:36 -0600

    nginx is not working with host network mode.

--- chute/run.sh
+++ chute/run.sh
@@ -2,13 +2,13 @@
 
 # Start the web server.  Its static files are located in /usr/share/nginx/html/
 # if you want to change the content.
-/etc/init.d/nginx start
+#/etc/init.d/nginx start
 
 # Make sure the BT interface is up.
 hciconfig hci0 up
 
 while true; do
-    hcitool scan >/usr/share/nginx/html/results.txt
+    hcitool scan
 done
 
 # If execution reaches this point, the chute will stop running.
